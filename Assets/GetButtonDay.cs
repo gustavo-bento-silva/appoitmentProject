0dc10b7cfe3ed2c6a0bd3281aa66c9519df08683
Gustavo Silva <gustavo.silva@movile.com>
2017-02-16 17:20:54 -0200

    GetButtonDay created

--- Assets/GetButtonDay.cs
+++ Assets/GetButtonDay.cs
@@ -1,13 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System;
 
 public class GetButtonDay : MonoBehaviour {
+
+	public CalendarPageController calendarPage;
 
 	public void OnButtonSelected()
 	{
 		CalendarController cpc = gameObject.transform.parent.parent.GetComponent<CalendarController>();
-		cpc.date = new System.DateTime(cpc.
-		gameObject.GetComponentInChildren<Text>().text ;
+		calendarPage.date = new System.DateTime(Int32.Parse(cpc._year.text), Int32.Parse(cpc._month.text), Int32.Parse(gameObject.GetComponentInChildren<Text>().text));
 	}
 }
