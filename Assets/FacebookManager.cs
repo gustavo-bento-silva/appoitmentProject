35466d2ba060fd5c9dfa8a5f3b80ed4b829f184a
Gustavo Silva <gustavo.silva@movile.com>
2018-04-27 13:35:57 -0300

    Build with facebook SDK

--- Assets/FacebookManager.cs
+++ Assets/FacebookManager.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Facebook.Unity;
+
+//using Facebook.Unity;
 
 public class FacebookManager : MonoBehaviour
 {
@@ -20,18 +21,18 @@ public class FacebookManager : MonoBehaviour
 		if (_instance == null) {
 			_instance = this;
 		}
-		if (!FB.IsInitialized) {
-			FB.Init (InitCallback);
-		}
+//		if (!FB.IsInitialized) {
+//			FB.Init (InitCallback);
+//		}
 	}
 
 	void InitCallback ()
 	{
-		if (FB.IsInitialized) {
-			Debug.Log ("Facebook SDK was successfully initialized");
-		} else {
-			Debug.Log ("Failed to Initialize the Facebook SDK");
-		}
+//		if (FB.IsInitialized) {
+//			Debug.Log ("Facebook SDK was successfully initialized");
+//		} else {
+//			Debug.Log ("Failed to Initialize the Facebook SDK");
+//		}
 	}
 
 	public void FacebookLogin (Delegates.UserLoginSuccess success, Delegates.UserLoginFail fail)
@@ -39,24 +40,24 @@ public class FacebookManager : MonoBehaviour
 		successCalback = success;
 		failcallback = fail;
 		var perms = new List<string> (){ "public_profile", "email" };
-		FB.LogInWithReadPermissions (perms, AuthCallback);
+//		FB.LogInWithReadPermissions (perms, AuthCallback);
 	}
 
-	private void AuthCallback (ILoginResult result)
+	private void AuthCallback (/*ILoginResult result*/)
 	{
-		if (FB.IsLoggedIn) {
-			// AccessToken class will have session details
-			var aToken = Facebook.Unity.AccessToken.CurrentAccessToken;
-			successCalback (aToken.UserId);
-			// Print current access token's User ID
-			Debug.Log (aToken.UserId);
-			// Print current access token's granted permissions
-			foreach (string perm in aToken.Permissions) {
-				Debug.Log (perm);
-			}
-		} else {
-			failcallback ("User cancelled login");
-			Debug.Log ("User cancelled login");
-		}
+//		if (FB.IsLoggedIn) {
+//			// AccessToken class will have session details
+//			var aToken = Facebook.Unity.AccessToken.CurrentAccessToken;
+//			successCalback (aToken.UserId);
+//			// Print current access token's User ID
+//			Debug.Log (aToken.UserId);
+//			// Print current access token's granted permissions
+//			foreach (string perm in aToken.Permissions) {
+//				Debug.Log (perm);
+//			}
+//		} else {
+//			failcallback ("User cancelled login");
+//			Debug.Log ("User cancelled login");
+//		}
 	}
 }
